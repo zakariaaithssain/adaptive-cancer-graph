0b61568ec253a442bfc93da12c4478b96052cb1d
Zakaria Ait Hssain <zakaria04aithssain@gmail.com>
2025-07-30 19:37:13 +0100

    add __pycache__ to .gitignore

--- scripts/api.py
+++ scripts/api.py
@@ -4,9 +4,6 @@ from config.apiconfig import API_KEY_EMAIL, QUERIES
 
 pubmed_api = PubMedAPI(api_key = API_KEY_EMAIL["api_key"], email = API_KEY_EMAIL["email"])
 
-
-
-
 all_articles = {}
 
 for cancer in QUERIES.keys():  
