9a3854733d9cf41359faccce642c0b8c7bdef9d2
Zakaria Ait Hssain <zakaria04aithssain@gmail.com>
2025-07-30 22:38:12 +0100

    get MPCid of fetched articles if available

--- scripts/api.py
+++ scripts/api.py
@@ -5,11 +5,17 @@ from config.apiconfig import API_KEY_EMAIL, QUERIES
 pubmed_api = PubMedAPI(api_key = API_KEY_EMAIL["api_key"], email = API_KEY_EMAIL["email"])
 
 all_articles = {}
-
+available_pmcid = 0
 for cancer in QUERIES.keys():  
     articles = pubmed_api.search_and_fetch(QUERIES[cancer], max_results=1000)
     all_articles[cancer] = articles
-    print(cancer, ":", len(all_articles[cancer]))
+    for article in articles: 
+        if article["pmcid"]: 
+            print(article["pmcid"])
+            available_pmcid+=1
+
+print("available PubMed Central Id in 1000*number_of_cancers articles: ", available_pmcid)
+    
 
 
 
