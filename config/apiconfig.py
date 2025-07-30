a16348f3b4df9aa487bcf7ee4d149a142b2bdfe2
Zakaria Ait Hssain <zakariaithssain@gmail.com>
2025-07-30 18:35:56 +0100

    add MPC API class to get articles body

--- config/apiconfig.py
+++ config/apiconfig.py
@@ -10,5 +10,24 @@ API_SLEEP_TIME = {"with_key" : 0.11,  #with key we have 10requests/second
                   "without_key" : 0.34 #without key we have 3requests/second
                   } 
 
+QUERIES = {       
+                   "stomach" :  '''("Stomach Neoplasms"[MeSH] OR "stomach cancer") AND  
+                                ("Genes"[MeSH] OR gene OR 
+                                    "Mutation"[MeSH] OR mutation OR 
+                                    "Risk Factors"[MeSH] OR "risk factor" OR 
+                                    "Drug Therapy"[Subheading] OR treatment OR 
+                                    "Tumor Cells"[MeSH] OR tumor OR 
+                                    "Cells"[MeSH] OR cell)''', 
+
+
+                    "prostate" : '''("Prostatic Neoplasms"[MeSH] OR "prostate cancer") AND     
+                                    ("Genes"[MeSH] OR gene OR 
+                                    "Mutation"[MeSH] OR mutation OR 
+                                    "Risk Factors"[MeSH] OR "risk factor" OR 
+                                    "Drug Therapy"[Subheading] OR treatment OR 
+                                    "Tumor Cells"[MeSH] OR tumor OR 
+                                    "Cells"[MeSH] OR cell)'''
+}
+
 
 
