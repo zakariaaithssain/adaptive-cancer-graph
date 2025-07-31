30e2d5f0a5f60528a533db5936e76ed86d2a9d78
Zakaria Ait Hssain <zakariaithssain@gmail.com>
2025-07-31 20:19:29 +0100

    add logging and tqdm

--- example_config/__init__.py
+++ example_config/__init__.py
@@ -1,1 +1,6 @@
+"""I cannot share personal config because it contains my API keys and connection string to my 
+mongo Atlas database. Before using any of this software, you should update the config files with
+your real data."""
+
 print("the config files are just examples and you should get real values for them.")
+
