035e3c2fc0815af41cfd2edd8ef54673eaf470be
Zakaria Ait Hssain <zakaria04aithssain@gmail.com>
2025-08-12 18:27:09 +0100

    add primal nlp script and config

--- data/unique.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("data/template.csv")
-
-unique = set(df.fromLabel.unique().tolist()).union(set(df.toLabel.unique().tolist()))
-print(list(unique))
-
-relations = df.relationship.unique()
-print(relations)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 
 from modules.pubmed_api import PubMedAPI
 from modules.pubmedcentral_api import PubMedCentralAPI
-from modules.load_to_mongo import APIsToMongo
+from modules.load_to_mongo import LoadToMongo
 
 from config.apis_config import API_KEY_EMAIL
 
@@ -10,7 +10,7 @@ from config.apis_config import API_KEY_EMAIL
 pubmed_api = PubMedAPI(api_key = API_KEY_EMAIL["api_key"], email = API_KEY_EMAIL["email"])
 pubmedcentral_api = PubMedCentralAPI(api_key = API_KEY_EMAIL["api_key"], email = API_KEY_EMAIL["email"])
 
-connector = APIsToMongo(pubmed_api=pubmed_api, pubmedcentral_api=pubmedcentral_api)
+connector = LoadToMongo(pubmed_api=pubmed_api, pubmedcentral_api=pubmedcentral_api)
 try: 
     #we can chain thanks to returning self from the 1st method. 
     connector.get_docs_from_apis(max_results=1000).insert_docs_to_mongo() 
