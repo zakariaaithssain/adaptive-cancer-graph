4f3f9dd878b21b27928d1435cb45d67e5a9d5669
Zakaria Ait Hssain <zakariaithssain@gmail.com>
2025-08-13 22:23:38 +0100

    implement a method to fetch docs from Atlas cloud

---
+++ extract.py
@@ -0,0 +1,7 @@
+from helpers.helpers import extract_pubmed_to_mongo
+
+
+#less max_results, less API pression, more loop iterations
+#if max results is not specified, the default is 1k, the max is 10k
+#extract_abstracts_only = False to get also bodies for each article if available in PubMedCentral
+extract_pubmed_to_mongo(extract_abstracts_only=True, max_results=1000)
--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,7 +1,11 @@
 """"this will contain helper functions for the project"""
+import logging 
 
-import logging
+from modules.pubmed_api import PubMedAPI
+from modules.pubmedcentral_api import PubMedCentralAPI
+from modules.mongo_connector import MongoAtlasConnector
 
+from config.apis_config import API_KEY_EMAIL
 from config.apis_config import QUERIES
 
 
@@ -64,3 +68,24 @@ def get_data_from_apis(pubmed_api, pubmedcentral_api, extract_abstracts_only = T
         else: logging.info(f"Helper: Finished Collecting Articles Abstracts.")
 
         return all_articles 
+
+
+#less max_results, less API pression, more loop iterations
+#if max results is not specified, the default is 1k, the max is 10k
+def extract_pubmed_to_mongo(extract_abstracts_only=True, max_results=1000):
+    pubmed_api = PubMedAPI(api_key = API_KEY_EMAIL["api_key"],
+                            email = API_KEY_EMAIL["email"])
+    
+    pubmedcentral_api = PubMedCentralAPI(api_key = API_KEY_EMAIL["api_key"],
+                                          email = API_KEY_EMAIL["email"])
+    connector = MongoAtlasConnector()
+    try: 
+        all_articles = get_data_from_apis(pubmed_api, pubmedcentral_api,
+                                        extract_abstracts_only,
+                                            max_results) 
+        
+        connector.load_articles_to_cloud(all_articles, abstract_only = True)
+
+    except KeyboardInterrupt: 
+        logging.error("Helper: Extraction Process Interrupted Manually.")
+        
