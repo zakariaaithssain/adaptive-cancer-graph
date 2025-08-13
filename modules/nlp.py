0e5af5320093e637f2ffeff30b820578ddfb5ad6
Zakaria Ait Hssain <zakaria04aithssain@gmail.com>
2025-08-13 13:28:53 +0100

    split NLP logic to NER and ER

--- modules/nlp.py
+++ modules/nlp.py
@@ -9,14 +9,13 @@ class NLP:
    def __init__(self):
       self.nlp_pipe = spacy.load("en_ner_bionlp13cg_md") 
       self.nlp_pipe.add_pipe("merge_entities", after="ner")
-
-
-
-   # Initialize matchers
+      self.entities= []
+      self.relations = []
+      #initialize matchers with model vocab
       self.matcher = Matcher(self.nlp_pipe.vocab)
       self.dep_matcher = DependencyMatcher(self.nlp_pipe.vocab)
 
-   # Add patterns to matchers
+      #add patterns to matchers
       for label, patterns in MATCHER_PATTERNS.items():
          self.matcher.add(label, patterns)
 
@@ -24,39 +23,45 @@ class NLP:
          self.dep_matcher.add(label, patterns) 
 
    
-
-   def extract_entities_and_relations(self, text):
+   #NER using en_ner_bionlp13cg_md scispacy model.
+   def extract_entities(self, text):
       doc = self.nlp_pipe(text)
-      entities = []
       for ent in doc.ents:
          if __name__ == "__main__": print(f"entity: {ent.text} --- label: {ent.label_}\n ******* ")
-         entities.append((ent.text, ent.label_))
+         self.entities.append((ent.text, ent.label_))
+      return self
 
-      relations = []
-
+#rule based and dependency based ER, so it's not that accurate like Model based ER.
+   def extract_relations(self, text): 
+      doc = self.nlp_pipe(text)
       # Matcher results
       matches = self.matcher(doc)
       for match_id, start, end in matches:
-         # Get entities fully inside the matched span
+         # get only relations related to entities recognized
          entities_in_span = [ent for ent in doc.ents if ent.start >= start and ent.end <= end]
          if len(entities_in_span) == 2:
                ent1, ent2 = entities_in_span
                relation = self.nlp_pipe.vocab.strings[match_id]
+               #print relations only if running as main
                if __name__ == "__main__": print(f"{ent1.text} -[{relation}]-> {ent2.text}\n ******* ")
-               relations.append((ent1.text, relation, ent2.text))
+               self.relations.append((ent1.text, relation, ent2.text))
+      
 
-      # Dependency matcher results
+      # dependency matcher results
       dep_matches = self.dep_matcher(doc)
       for match_id, token_ids in dep_matches:
          relation = self.nlp_pipe.vocab.strings[match_id]
          ent1 = doc[token_ids[0]]
          ent2 = doc[token_ids[-1]]
+         #print relations only if running as main
          if __name__ == "__main__": print(f"{ent1.text} -[{relation}]-> {ent2.text}\n ******* ")
-         relations.append((ent1.text, relation, ent2.text))
+         self.relations.append((ent1.text, relation, ent2.text))
 
-      return {"entities": entities, "relations": relations}
+      return self
 
+#printing entities recognized by the model if running as main.
 if __name__ == "__main__": 
    nlp = NLP() 
+   print("Entities recognized by ner_bionlp13cg_md model:")
    print(nlp.nlp_pipe.get_pipe("ner").labels)
 
